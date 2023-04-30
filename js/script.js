53ff05066404c36c52ce1d0378e8bc9681d2ff4d
kritgrover <kritgrover@gmail.com>
2023-04-30 08:19:45 -0400

    minor debugging

--- js/script.js
+++ js/script.js
@@ -1,13 +1,6 @@
-const formOpenBtn = document.querySelector("#form-open"),
-    home = document.querySelector(".home"),
-    formContainer = document.querySelector(".form_container"),
-    formCloseBtn = document.querySelector(".form_close"),
-    signupBtn = document.querySelector("#signup"),
+const formContainer = document.querySelector(".form_container"),
     loginBtn = document.querySelector("#login"),
     pwShowHide = document.querySelectorAll(".pw_hide");
-
-formOpenBtn.addEventListener("click", () => home.classList.add("show"));
-formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
 
 pwShowHide.forEach((icon) => {
     icon.addEventListener("click", () => {
@@ -22,10 +15,6 @@ pwShowHide.forEach((icon) => {
     });
 });
 
-signupBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    formContainer.classList.add("active");
-});
 loginBtn.addEventListener("click", (e) => {
     e.preventDefault();
     formContainer.classList.remove("active");
